9f1779b0c0f724563753c23e4b132684137a16aa
Nick Rupp <nickrupp95@gmail.com>
2024-12-29 20:05:33 +0000

    export key types and builder functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,18 @@
 export {FidgetSpinner} from './FidgetSpinner';
+
+export {Sparks} from './FidgetSpinner/Sparks';
+export {type SparkConfig, buildSparkConfig} from './FidgetSpinner/SparkConfig';
+
+export {Bubbles} from './FidgetSpinner/Bubbles';
+export {type BubbleConfig, buildBubbleConfig} from './FidgetSpinner/BubbleConfig';
+
+export {type ResetConfig, buildResetConfig} from './FidgetSpinner/ResetConfig';
+
+export {type ScaleConfig, buildScaleConfig} from './FidgetSpinner/ScaleConfig';
+
+export {
+    type VelocityBreakpointConfig,
+    type VelocityBreakpoint,
+    type VelocityBreakpoints,
+    buildVelocityBreakpoints as buildVelocityBreakpointConfigs,
+} from './FidgetSpinner/VelocityBreakpoints';
