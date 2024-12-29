fc79c73d5353e3221f4c64c36a46e3f0808fabe7
Nick Rupp <nickrupp95@gmail.com>
2024-12-29 18:18:39 +0000

    remove lodash as a dependency

--- templates/Component/TemplateName.tsx
+++ templates/Component/TemplateName.tsx
@@ -1,6 +1,5 @@
 import type {FC} from 'react';
 import {useEffect} from 'react';
-import {throttle} from 'lodash';
 
 import classes from './TemplateName.module.css';
 import useLogic from './useLogic';
@@ -14,9 +13,9 @@ export const TemplateName: FC<Props> = ({initialValue = 0}) => {
     const {count, incrementCount} = useLogic(initialValue);
 
     useEffect(() => {
-        const runner = throttle(() => {
+        const runner = () => {
             console.log('throttle');
-        }, 10);
+        };
         runner();
     }, []);
     return (
